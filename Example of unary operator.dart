676e3ac0a5f78833218560af3813bfd14ea46d7a
w3-sakib <84985523+w3-sakib@users.noreply.github.com>
2023-05-04 17:13:20 +0600

    Add files via upload

---
+++ Example of unary operator.dart
@@ -0,0 +1,36 @@
+void main(){
+  var x=10;
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+  print(x++);
+
+  var z=10;
+
+  print(++z);
+  print(++z);
+  print(++z);
+  print(++z);
+  print(++z);
+
+  var y=6;
+  print(y--);
+  print(y--);
+  print(y--);
+  print(y--);
+  print(y--);
+  print(y--);
+
+  var h=6;
+  print(--h);
+  print(--h);
+  print(--h);
+  print(--h);
+  print(--h);
+  print(--h);
+  print(--h);
+
+
+}
